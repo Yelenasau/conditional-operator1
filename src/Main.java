16f11cbf60d9b2088da5ca42c86931e60da53318
Yelenasau <elenasauschkina@mail.ru>
2023-04-26 23:21:18 +0400

    дз условный оператор 2

--- src/Main.java
+++ src/Main.java
@@ -26,7 +26,8 @@ public class Main {
         }
         if (temp > 5 ) {
         System.out.println("На улице " + temp + " градусов, можно идти без шапки.");
-} }
+        }
+    }
     public static void task3() {
         System.out.println("Задача 3");
         int speed = 67;
@@ -35,52 +36,62 @@ public class Main {
         }
         if ( speed < 60 ) {
             System.out.println("Если скорость " + speed + " то можно ездить спокойно.");
-        } }
+        }
+    }
     public static void task4() {
         System.out.println("Задача 4");
-        int age = 24;
+        int age = 32;
         if (age > 0 && age < 2) {
         System.out.println("Если возраст человека равен " + age + " то ему пора спать.");
-    }
-        if (age >= 2 && age <= 6) {
+        }
+        else if (age >= 2 && age <= 6) {
             System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в детский сад.");
         }
-        if (age >= 7 && age <= 18) {
+        else if (age >= 7 && age <= 18) {
             System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в школу.");
         }
-        if (age > 18 && age <= 24) {
+        else if (age > 18 && age <= 24) {
             System.out.println("Если возраст человека равен " + age + " то его место в университете.");
         }
-        if (age > 24 && age <= 60) {
+        else if (age > 24 && age <= 60) {
             System.out.println("Если возраст человека равен " + age + " то ему пора ходить на работу.");
         }
-        if (age > 60 ) {
+        else if (age > 60 ) {
             System.out.println("Если возраст человека равен " + age + " то он может отдохнуть.");
-        } }
-        public static void task5() {
-            System.out.println("Задача 5");
-            int age = 14;
-            if (age < 5 ) {
-                System.out.println("Если возраст ребенка равен " + age + " то ему нельзя кататься на аттракционе.");
-            }
-                if (age > 5 && age < 14) {
-                    System.out.println("Если возраст ребенка равен " + age + " то он может кататься на аттракционе только в сопровождении взрослого.");
-                }
-            if (age >= 14) {
-                System.out.println("Если возраст ребенка равен " + age + " то он может кататься на аттракционе без сопровождения взрослого.");
-            }
-}       public static void task6() {
-        System.out.println("Задача 6");
-        int place = 103;
-        if (place > 1 && place <=60) {
-            System.out.println("В вагоне есть сидячие места.");}
-        if (place > 60 && place <= 102) {
-            System.out.println("В вагоне есть стоячие места.");}
-        if ( place < 103) {System.out.println();
-        } else {
-                System.out.println("Мест нет.");
-            }
         }
+    }
+        public static void task5() {
+        System.out.println("Задача 5");
+        int age = 14;
+        if (age < 5 ) {
+            System.out.println("Если возраст ребенка равен " + age + " то ему нельзя кататься на аттракционе.");
+        }
+        else if (age > 5 && age < 14) {
+            System.out.println("Если возраст ребенка равен " + age + " то он может кататься на аттракционе только в сопровождении взрослого.");
+        }
+        else if (age >= 14) {
+            System.out.println("Если возраст ребенка равен " + age + " то он может кататься на аттракционе без сопровождения взрослого.");
+        }
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        int plase = 102;
+        int seatPlace = 60;
+        int standingRoom = (plase - seatPlace);
+
+        int seatUser =45;
+        int standingUser = 42;
+        if (seatPlace > seatUser) {
+            System.out.println("В вагоне есть сидячие места.");
+        } else {
+            System.out.println("Мест нет.");
+        }
+        if (standingUser < standingRoom) {
+            System.out.println("В вагоне есть стоячие места.");
+        } else {
+            System.out.println("Мест нет.");
+        }
+    }
     public static void task7() {
         System.out.println("Задача 7");
         int one = 1;
@@ -91,5 +102,5 @@ public class Main {
        } else {
            System.out.println("Большее число " + two);
        }
-}
     }
+}
